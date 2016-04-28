1ce9d55419d692157dc48d2adf44e1c2875c0fb8
Rob <rhlowe@gmail.com>
2016-04-27 20:51:43 -0500

    test file comit

---
+++ tests/integration/components/text-placeholder-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('text-placeholder', 'Integration | Component | text placeholder', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{text-placeholder}}`);
+
+  assert.equal(this.$().text().trim(), '');
+});
